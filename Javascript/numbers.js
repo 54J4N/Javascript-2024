b0175056b76f503a11def08f7cc678c32270b9b6
Sajan Ghimire <122589374+54J4N@users.noreply.github.com>
2024-09-18 14:27:11 +0545

    integers and floats

---
+++ Javascript/numbers.js
@@ -0,0 +1,12 @@
+//Two type of numbers - 1. Integers -2.Floats
+//Integer 
+let x = 1;
+let y = 2;
+console.log(x + y);
+
+//Float: refers to the num with decimal points like this :
+let r = 4.5;
+let s = 5.6;
+console.log(r + s);
+//To create a float,we need to write a number and use . to define the decimal values.
+
