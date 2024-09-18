cee7e7295f1bc4788a383fe40c1e7d39fedca78c
Sajan Ghimire <122589374+54J4N@users.noreply.github.com>
2024-09-18 14:29:23 +0545

    try to represent a var that hasn't been assigned any value yet

---
+++ Javascript/undefined.js
@@ -0,0 +1,6 @@
+//A datatype in js used to represent a variable that hasn't been assigned any value yet.
+let first_name;
+console.log(first_name); //undfined
+//also assign  undefined to a variable explicily as follow:
+let last_name = undefined;
+//but this is usually not recommended ,bcs js has another value called null which is used to make a variiable as empty.
