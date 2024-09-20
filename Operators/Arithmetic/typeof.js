c73de8457af2ff0cc7e79c6b11e558c12237c21e
Sajan Ghimire <122589374+54J4N@users.noreply.github.com>
2024-09-20 15:55:36 +0545

    usage of typeof operator to determine variable data types

---
+++ Operators/Arithmetic/typeof.js
@@ -0,0 +1,20 @@
+//Used to determine the data type of a variable or value .
+
+let x = 5;
+console.log(typeof x);          // Output: 'number'
+
+console.log(typeof "unisha");    // Output: 'string'
+
+console.log(typeof true);        // Output: 'boolean'
+
+let y;
+console.log(typeof y);          // Output: 'undefined'
+
+let obj = { name: "bhesh" };
+console.log(typeof obj);        // Output: 'object'
+
+let arr = [1, 2, 3];
+console.log(typeof arr);        // Output: 'object'
+
+function myFunction() {}
+console.log(typeof myFunction);  // Output: 'function'
